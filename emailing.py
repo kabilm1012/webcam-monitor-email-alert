ad81bfc8b77c17d12d2a26348fcba5e5145f0d1e
Kabil M <kabilm1012@gmail.com>
2024-09-26 11:12:39 +0530

    Email with attachment

--- emailing.py
+++ emailing.py
@@ -1,3 +1,33 @@
-def send_email():
-    print("Email was sent successfully!")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+import os
+
+SENDER = "guestkabil@gmail.com"
+PASSWORD = os.getenv("PASSWORD")
+RECEIVER = "kabilm1012@gmail.com"
+
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a New customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email("images/28.png")
+
+    
     
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import cv2 as cv
 import time
 from emailing import send_email
+import glob
 
 video = cv.VideoCapture(0)
 time.sleep(1)
 first_frame = None
 status_list = []
+count = 1
 
 while True:
     status = 0
     check, frame = video.read()
+    
     gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray_frame_gau = cv.GaussianBlur(gray_frame, (11, 11), 0)
 
@@ -30,11 +33,16 @@ while True:
         rectangle = cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images/2))
+            best_image_with_object = all_images[index]
         
     status_list.append(status)
     status_list = status_list[-2:]
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(best_image_with_object)
     print(status_list)
 
     cv.imshow("Video", frame)
